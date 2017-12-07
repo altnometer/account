3c8fb845c6b5b8e85b055c2b37ed7a64a3935e53
sam <sam@redmoo.info>
2017-12-07 10:43:58 +0300

    Moved BeforeEach() to Context scope

--- mw/db_test.go
+++ mw/db_test.go
@@ -20,24 +20,24 @@ var _ = Describe("WithDB", func() {
 		mHand func() http.Handler // mock handler
 		h     http.Handler
 	)
-	BeforeEach(func() {
-		mDB = mocks.BoltClient{}
-		mDB.GetCall.Receives.Name = "username"
-		mDB.GetCall.Returns.ID = []byte("12345")
-		mDB.GetCall.Returns.Error = nil
-		w = httptest.NewRecorder()
-		mHand = func() http.Handler {
-			fn := func(w http.ResponseWriter, r *http.Request) {
-				_, ok := context.GetOk(r, "db")
-				if !ok {
-					panic("failed to get db from context")
-				}
-			}
-			return http.HandlerFunc(fn)
-		}
-		h = mw.WithDB(&mDB, mHand())
-	})
 	Context("when wraps an http.Handler", func() {
+		BeforeEach(func() {
+			mDB = mocks.BoltClient{}
+			mDB.GetCall.Receives.Name = "username"
+			mDB.GetCall.Returns.ID = []byte("12345")
+			mDB.GetCall.Returns.Error = nil
+			w = httptest.NewRecorder()
+			mHand = func() http.Handler {
+				fn := func(w http.ResponseWriter, r *http.Request) {
+					_, ok := context.GetOk(r, "db")
+					if !ok {
+						panic("failed to get db from context")
+					}
+				}
+				return http.HandlerFunc(fn)
+			}
+			h = mw.WithDB(&mDB, mHand())
+		})
 		It("passes a db client in context", func() {
 			h.ServeHTTP(w, r)
 			db, ok := context.GetOk(r, "db")
