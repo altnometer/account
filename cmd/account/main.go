c56639b9a586ffd350f46559475fd6b68d25e594
sam <sam@redmoo.info>
2017-12-20 17:13:24 +0300

    Temp debug kafka producer with stdin api

--- cmd/account/main.go
+++ cmd/account/main.go
@@ -1,13 +1,47 @@
 package main
 
 import (
-	"flag"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 
-	"github.com/altnometer/account/service"
+	"github.com/altnometer/account/kafka"
 )
 
 func main() {
-	port := flag.String("port", "8080", "server port")
-	flag.Parse()
-	service.StartWebServer(*port)
+	// port := flag.String("port", "8080", "server port")
+	// flag.Parse()
+	reader := bufio.NewReader(os.Stdin)
+
+	kp := kafka.SyncProducer{}
+	err := kp.InitMySyncProducer()
+	if err != nil {
+		panic(err)
+	}
+	// kp.SendAccMsg([]byte("hello"))
+	// service.StartWebServer(*port)
+	fmt.Print("-> ")
+	text, _ := reader.ReadString('\n')
+	text = strings.Replace(text, "\n", "", -1)
+	args := strings.Split(text, "###")
+	cmd := args[0]
+
+	switch cmd {
+	case "write":
+		if len(args) == 2 {
+			msg := args[1]
+			// event := NewCreateAccountEvent(accName)
+			kp.SendAccMsg([]byte(msg))
+		} else {
+			fmt.Println("Only specify write###yourmessage")
+		}
+	default:
+		fmt.Printf("Unknown command %s, only: write, is implementd.\n", cmd)
+	}
+
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		err = nil
+	}
 }
