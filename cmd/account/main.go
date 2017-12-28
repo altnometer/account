68d0a3d103d54aa218fbaa2033282795b96f93a1
sam <sam@redmoo.info>
2017-12-28 11:53:43 +0300

    Commented out kafka part in main() for avoid failing a test.

--- cmd/account/main.go
+++ cmd/account/main.go
@@ -1,47 +1,43 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+	"flag"
 
-	"github.com/altnometer/account/kafka"
+	"github.com/altnometer/account/service"
 )
 
 func main() {
-	// port := flag.String("port", "8080", "server port")
-	// flag.Parse()
-	reader := bufio.NewReader(os.Stdin)
-
-	kp := kafka.SyncProducer{}
-	err := kp.InitMySyncProducer()
-	if err != nil {
-		panic(err)
-	}
+	port := flag.String("port", "8080", "server port")
+	flag.Parse()
+	service.StartWebServer(*port)
+	// reader := bufio.NewReader(os.Stdin)
+	// kp := kafka.SyncProducer{}
+	// err := kp.InitMySyncProducer()
+	// if err != nil {
+	// 	panic(err)
+	// }
 	// kp.SendAccMsg([]byte("hello"))
-	// service.StartWebServer(*port)
-	fmt.Print("-> ")
-	text, _ := reader.ReadString('\n')
-	text = strings.Replace(text, "\n", "", -1)
-	args := strings.Split(text, "###")
-	cmd := args[0]
+	// fmt.Print("-> ")
+	// text, _ := reader.ReadString('\n')
+	// text = strings.Replace(text, "\n", "", -1)
+	// args := strings.Split(text, "###")
+	// cmd := args[0]
 
-	switch cmd {
-	case "write":
-		if len(args) == 2 {
-			msg := args[1]
-			// event := NewCreateAccountEvent(accName)
-			kp.SendAccMsg([]byte(msg))
-		} else {
-			fmt.Println("Only specify write###yourmessage")
-		}
-	default:
-		fmt.Printf("Unknown command %s, only: write, is implementd.\n", cmd)
-	}
+	// switch cmd {
+	// case "write":
+	// 	if len(args) == 2 {
+	// 		msg := args[1]
+	// 		// event := NewCreateAccountEvent(accName)
+	// 		kp.SendAccMsg([]byte(msg))
+	// 	} else {
+	// 		fmt.Println("Only specify write###yourmessage")
+	// 	}
+	// default:
+	// 	fmt.Printf("Unknown command %s, only: write, is implementd.\n", cmd)
+	// }
 
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		err = nil
-	}
+	// if err != nil {
+	// 	fmt.Printf("Error: %s\n", err)
+	// 	err = nil
+	// }
 }
