970eb8a84f54af8574a37655faff2b22e97391ef
sam <sam@redmoo.info>
2017-12-29 19:28:34 +0300

    Moved account.go to model/account.go

--- model/account.go
+++ model/account.go
@@ -1,7 +1,37 @@
-package model
+package account
 
-// Account store user details
+import "sync"
+
+// Account holds core user details.
 type Account struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
+	Pwd  string `json:"pwd"`
+}
+
+type uNameSet struct {
+	sync.RWMutex
+	m map[string]struct{}
+}
+
+func (ns *uNameSet) isInSet(name string) bool {
+	ns.RLock()
+	defer ns.RUnlock()
+	if _, ok := ns.m[name]; !ok {
+		return false
+	}
+	return true
+}
+
+func (ns *uNameSet) addToSet(name string) {
+	ns.Lock()
+	defer ns.Unlock()
+	ns.m[name] = struct{}{}
+}
+
+var uNames = uNameSet{m: make(map[string]struct{})}
+
+// NameExists  incapsulates duplicate name checking api.
+var NameExists = func(name string) bool {
+	return uNames.isInSet(name)
 }
