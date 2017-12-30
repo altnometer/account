69c813f499952efc60da2a9bf9d0bdfd7c5e7c45
sam <sam@redmoo.info>
2017-12-30 12:43:33 +0300

    Changed SendAccMsg() param to model.Account
    Modified/added saram.ProducerMessage() args.

--- kafka/producer.go
+++ kafka/producer.go
@@ -7,13 +7,15 @@ import (
 	"os"
 	"os/signal"
 	"strings"
+	"time"
+	"try/goblog/accountservice/model"
 
 	"github.com/Shopify/sarama"
 )
 
 // ISyncProducer interacts with kafka brokers as a producer.
 type ISyncProducer interface {
-	SendAccMsg([]byte) error
+	SendAccMsg(*model.Account) error
 }
 
 // SyncProducer implement ISyncProducer interface.
@@ -72,16 +74,17 @@ func (p *SyncProducer) InitMySyncProducer() error {
 }
 
 // SendAccMsg sends kafka message.
-func (p *SyncProducer) SendAccMsg(msg []byte) error {
-	json, err := json.Marshal(msg)
+func (p *SyncProducer) SendAccMsg(acc *model.Account) error {
+	accBytes, err := json.Marshal(acc)
 
 	if err != nil {
 		return err
 	}
 	msgLog := sarama.ProducerMessage{
-		Topic: topic,
-		// Value: sarama.StringEncoder("some_string"),
-		Value: sarama.ByteEncoder(json),
+		Topic:     topic,
+		Key:       sarama.StringEncoder(acc.ID),
+		Timestamp: time.Now(),
+		Value:     sarama.ByteEncoder(accBytes),
 	}
 	// partition, offset, err := kafka.SendMessage(&msgLog)
 	_, _, err = p.Producer.SendMessage(&msgLog)
@@ -90,7 +93,5 @@ func (p *SyncProducer) SendAccMsg(msg []byte) error {
 	}
 	// fmt.Printf("Message is stored in partition %d, offset %d\n",
 	// partition, offset)
-
 	return nil
-
 }
