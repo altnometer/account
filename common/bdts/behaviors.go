0ebca3f5ea6a2157f29312d40c8e959dc10b2948
sam <sam@redmoo.info>
2018-01-03 13:20:19 +0300

    Added common testing behavior assertion and godocs
    Added AssertRespBodyContains() to assert the response body
    contains a string.

--- common/bdts/behaviors.go
+++ common/bdts/behaviors.go
@@ -1,4 +1,4 @@
-// contains ginkgo shared behavior tests
+// Package bdts (Behavior Driven Tests) contains ginkgo shared behavior tests
 // of the form It("should perform this way", behaviors.AssertThisBehavior())
 package bdts
 
@@ -8,19 +8,30 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+// TestHttpRespCodeAndBody refactors testing response body and status code.
 type TestHttpRespCodeAndBody struct {
 	W    *httptest.ResponseRecorder
 	Code int
 	Body string
 }
 
+// AssertStatusCode tests status code of a response.
 func AssertStatusCode(inputs *TestHttpRespCodeAndBody) func() {
 	return func() {
 		Expect(inputs.W.Code).To(Equal(inputs.Code))
 	}
 }
+
+// AssertRespBody tests if response body is a string.
 func AssertRespBody(inputs *TestHttpRespCodeAndBody) func() {
 	return func() {
 		Expect(inputs.W.Body.String()).To(Equal(inputs.Body + "\n"))
+	}
+}
+
+// AssertRespBodyContains tests if response body contains a string.
+func AssertRespBodyContains(inputs *TestHttpRespCodeAndBody) func() {
+	return func() {
+		Expect(inputs.W.Body.String()).Should(ContainSubstring(inputs.Body))
 	}
 }
