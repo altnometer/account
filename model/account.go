2977d3f48a967c4b6205026af5d8bc179e4d5a18
sam <sam@redmoo.info>
2018-01-17 11:12:32 +0300

    Moved register form code to regform.go

--- model/account.go
+++ model/account.go
@@ -2,39 +2,6 @@ package model
 
 import "sync"
 
-// MaxUserNameLength limits username length in characters.
-const MaxUserNameLength = 32
-
-// MaxPasswordLength limits pwd length in characters.
-const MaxPasswordLength = 128
-
-// MinPasswordLength limits pwd length in characters.
-const MinPasswordLength = 6
-
-// ReservedUsernames must not be part of a usernames.
-var ReservedUsernames = [...]string{
-	"admin",
-	"redmoo",
-	"supervisor",
-}
-
-// FormOKer implements OK method.
-type FormOKer interface {
-	OK() (int, error)
-}
-
-// RegForm holds field names for a register form.
-type RegForm struct {
-	Name    string `json:"name"`
-	Pwd     string `json:"pwd"`
-	PwdConf string `json:"pwd_conf"`
-}
-
-// OK checks form fields and returns a status code and error.
-func (f *RegForm) OK() (int, error) {
-	return 200, nil
-}
-
 // Account holds core user details.
 type Account struct {
 	ID      string `json:"id"`
