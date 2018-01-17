32aab7006efdfc6740bd2dce2176b7110f1a8e18
sam <sam@redmoo.info>
2018-01-17 18:58:09 +0300

    Moved tesing register form to model package

--- model/regform.go
+++ model/regform.go
@@ -1,5 +1,12 @@
 package model
 
+import (
+	"errors"
+	"net/http"
+	"strings"
+	"unicode/utf8"
+)
+
 // MaxUserNameLength limits username length in characters.
 const MaxUserNameLength = 32
 
@@ -30,7 +37,62 @@ type RegForm struct {
 
 // OK checks form fields and returns a status code and error.
 func (f *RegForm) OK() (int, error) {
+	if f.Pwd != f.PwdConf {
+		return http.StatusBadRequest, errors.New("PWD_PWDCONF_NO_MATCH")
+	}
+	if code, err := checkReservedUnames(f.Name); err != nil {
+		return code, err
+	}
+	if code, err := checkUNameLength(f.Name); err != nil {
+		return code, err
+	}
+	if code, err := checkPWDLength(f.Pwd); err != nil {
+		return code, err
+	}
+	if code, err := checkUNameIsValidUTF8(f.Name); err != nil {
+		return code, err
+	}
+	if code, err := checkNewLineChars(f.Name); err != nil {
+		return code, err
+	}
+	if UNameExists(f.Name) {
+		return 400, errors.New("NAME_ALREADY_EXISTS")
+	}
+
 	return 200, nil
 }
-
-
+func checkReservedUnames(uname string) (int, error) {
+	for _, n := range ReservedUsernames {
+		if strings.Contains(uname, n) {
+			return 400, errors.New("NO_RESERVED_NAMES_ALLOWED")
+		}
+	}
+	return 200, nil
+}
+func checkUNameLength(uname string) (int, error) {
+	if utf8.RuneCountInString(uname) > MaxUserNameLength {
+		return 400, errors.New("NAME_TOO_LONG")
+	}
+	return 400, nil
+}
+func checkPWDLength(pwd string) (int, error) {
+	if utf8.RuneCountInString(pwd) > MaxPasswordLength {
+		return 400, errors.New("PWD_TOO_LONG")
+	}
+	if utf8.RuneCountInString(pwd) < MinPasswordLength {
+		return 400, errors.New("ARG_PWD_TOO_SHORT")
+	}
+	return 200, nil
+}
+func checkUNameIsValidUTF8(uname string) (int, error) {
+	if !utf8.ValidString(uname) {
+		return 400, errors.New("NAME_INVALID_UTF8_STRING")
+	}
+	return 200, nil
+}
+func checkNewLineChars(uname string) (int, error) {
+	if strings.Contains(uname, "\n") {
+		return 400, errors.New("NAME_NEWLINE_NOT_ALLOWED")
+	}
+	return 200, nil
+}
---
+++ model/regform_test.go
@@ -0,0 +1,127 @@
+package model_test
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+
+	"github.com/altnometer/account/model"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("FormOKer", func() {
+	var (
+		f                  model.RegForm
+		name, pwd, pwdConf string
+		uNameExistsBefore  func(name string) bool
+	)
+	BeforeEach(func() {
+		name = "unameЯйцоЖЭ"
+		pwd = "ka88dk#яфэюж"
+		pwdConf = "ka88dk#яфэюж"
+		uNameExistsBefore = model.UNameExists
+	})
+	AfterEach(func() {
+		model.UNameExists = uNameExistsBefore
+	})
+
+	JustBeforeEach(func() {
+		f = model.RegForm{name, pwd, pwdConf}
+	})
+	Context("Pwd and PwdConf do not match", func() {
+		BeforeEach(func() {
+			pwdConf = "abc"
+		})
+		It("returns 400 code and error", func() {
+			code, err := f.OK()
+			Expect(code).To(Equal(400))
+			Expect(err.Error()).To(ContainSubstring("PWD_PWDCONF_NO_MATCH"))
+		})
+	})
+	Context("username contains a reserved name", func() {
+		rand.Seed(time.Now().Unix())
+		rns := model.ReservedUsernames
+		uname := fmt.Sprintf("z%sж", rns[rand.Intn(len(rns)-1)])
+		// uname := rns[rand.Intn(len(rns)-1)]
+		BeforeEach(func() {
+			name = uname
+		})
+		It("returns 400 code and error", func() {
+			code, err := f.OK()
+			Expect(code).To(Equal(400))
+			Expect(err.Error()).To(ContainSubstring("NO_RESERVED_NAMES_ALLOWED"))
+		})
+
+	})
+	Context("username exceeds max length", func() {
+		uname := strings.Repeat("й", model.MaxUserNameLength+1)
+		BeforeEach(func() {
+			name = uname
+		})
+		It("returns 400 code and error", func() {
+			code, err := f.OK()
+			Expect(code).To(Equal(400))
+			Expect(err.Error()).To(ContainSubstring("NAME_TOO_LONG"))
+		})
+	})
+	Context("password exceeds max length", func() {
+		pwdLong := strings.Repeat("й", model.MaxPasswordLength+1)
+		BeforeEach(func() {
+			pwd = pwdLong
+			pwdConf = pwdLong
+		})
+		It("returns 400 code and error", func() {
+			code, err := f.OK()
+			Expect(code).To(Equal(400))
+			Expect(err.Error()).To(ContainSubstring("PWD_TOO_LONG"))
+		})
+	})
+	Context("password is less than min length", func() {
+		pwdShort := strings.Repeat("й", model.MinPasswordLength-1)
+		BeforeEach(func() {
+			pwd = pwdShort
+			pwdConf = pwdShort
+		})
+		It("returns 400 code and error", func() {
+			code, err := f.OK()
+			Expect(code).To(Equal(400))
+			Expect(err.Error()).To(ContainSubstring("PWD_TOO_SHORT"))
+		})
+	})
+	Context("username is an invalid utf8 string", func() {
+		uname := "zйфж\xbd"
+		BeforeEach(func() {
+			name = uname
+		})
+		It("returns 400 code and error", func() {
+			code, err := f.OK()
+			Expect(code).To(Equal(400))
+			Expect(err.Error()).To(ContainSubstring("NAME_INVALID_UTF8_STRING"))
+		})
+	})
+	Context("username contains new line char", func() {
+		uname := "zйфж\n"
+		BeforeEach(func() {
+			name = uname
+		})
+		It("returns 400 code and error", func() {
+			code, err := f.OK()
+			Expect(code).To(Equal(400))
+			Expect(err.Error()).To(ContainSubstring("NAME_NEWLINE_NOT_ALLOWED"))
+		})
+	})
+	Context("when username already exists", func() {
+		BeforeEach(func() {
+			model.UNameExists = func(string) bool {
+				return true
+			}
+		})
+		It("returns 400 code and error", func() {
+			code, err := f.OK()
+			Expect(code).To(Equal(400))
+			Expect(err.Error()).To(ContainSubstring("NAME_ALREADY_EXISTS"))
+		})
+	})
+})
