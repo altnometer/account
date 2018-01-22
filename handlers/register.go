d14d6a3744224a54e5fae8477525af1a427864be
sam <sam@redmoo.info>
2018-01-22 19:06:54 +0300

    Changed sending Acc data to kafka from Register
    Under new api,
    Register handler calls SendToKafka() Acc method.
    The old way was to call sendAccKafkaMsg().

--- handlers/register.go
+++ handlers/register.go
@@ -1,14 +1,10 @@
 package handlers
 
 import (
-	"errors"
-	"fmt"
 	"net/http"
 
-	"github.com/altnometer/account/kafka"
 	"github.com/altnometer/account/model"
-
-	"github.com/gorilla/context"
+	"github.com/altnometer/account/mw"
 )
 
 // Register holds data used in ServeHTTP method for user registration.
@@ -19,33 +15,20 @@ type Register struct {
 
 // Register handles an HTTP request to register a user.
 func (reg *Register) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	name := r.PostFormValue("Name")
-	pwd := r.PostFormValue("Pwd")
-	if len(name) == 0 || len(pwd) == 0 {
-		// okregform middleware would not let this panic happen.
-		// it only checks that unit tests naming is valid.
-		panic("No name or pwd args in register form")
-	}
-	accData, err := model.NewAcc(name, pwd)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	if code, err := sendAccKafkaMsg(accData, r); err != nil {
-		http.Error(w, err.Error(), code)
-		return
-	}
-	http.Redirect(w, r, reg.RedirectURL, reg.StatusCode)
-}
-
-func sendAccKafkaMsg(acc *model.Account, r *http.Request) (int, error) {
-	k, ok := context.GetOk(r, "kfkProdr")
-	if !ok {
-		return 500, errors.New("NO_KAFKA_PRODUCER_IN_CONTEXT")
-	}
-	kp := k.(kafka.ISyncProducer)
-	if err := kp.SendAccMsg(acc); err != nil {
-		return 500, fmt.Errorf("FAILED_KAFKA_MSG_SEND: %s", err.Error())
-	}
-	return 200, nil
+	sendKafkaMsg := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		accData, err := model.NewAcc(
+			r.PostFormValue("Name"),
+			r.PostFormValue("Pwd"))
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		if err := accData.SendToKafka(r); err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		http.Redirect(w, r, reg.RedirectURL, reg.StatusCode)
+	})
+	h := mw.WithKafkaProducer(mw.OKRegForm(sendKafkaMsg, &model.RegForm{}))
+	h.ServeHTTP(w, r)
 }
