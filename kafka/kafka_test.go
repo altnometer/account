2cb28045d7fdde9df31afce34476c7b4877ed2b2
sam <sam@redmoo.info>
2018-01-22 10:08:51 +0300

    Modified kafka_test for NewSyncProducer() addition

--- kafka/kafka_test.go
+++ kafka/kafka_test.go
@@ -11,38 +11,25 @@ import (
 var _ = Describe("Kafka", func() {
 	Describe("InitMySyncProducer", func() {
 		var (
-			p             kafka.SyncProducer
 			brokersEnvVar string
 		)
 		BeforeEach(func() {
-			p = kafka.SyncProducer{}
 			brokersEnvVar = "127.0.0.1:9092,127.0.0.1:9092"
 		})
 		JustBeforeEach(func() {
 			os.Setenv("KAFKA_BROKERS", brokersEnvVar)
 		})
-		Context("when KAFKA_BROKERS env var is an empty string", func() {
-			BeforeEach(func() {
-				brokersEnvVar = ""
-			})
-			It("returns an error", func() {
-
-				err := p.InitMySyncProducer()
-				Expect(err).Should(HaveOccurred())
-				Expect(err.Error()).To(Equal("NO_KAFKA_BROKERS_ARG_IN_ENV"))
-
-			})
-		})
 		Context("when no KAFKA_BROKERS env var is set", func() {
 			JustBeforeEach(func() {
 				os.Unsetenv("KAFKA_BROKERS")
 			})
-			It("returns an error", func() {
-
-				err := p.InitMySyncProducer()
-				Expect(err).Should(HaveOccurred())
-				Expect(err.Error()).To(Equal("NO_KAFKA_BROKERS_ARG_IN_ENV"))
-
+			It("panics with a correct msg", func() {
+				defer func() {
+					r := recover()
+					Expect(r).NotTo(BeNil())
+					Expect(r).Should(ContainSubstring("NO_KAFKA_BROKERS_ARG_IN_ENV"))
+				}()
+				_ = kafka.NewSyncProducer()
 			})
 		})
 	})
