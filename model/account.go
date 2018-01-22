79cb4969427ccf5931ded849cd16f7be4dae67ea
sam <sam@redmoo.info>
2018-01-22 18:40:06 +0300

    Added SendToKafka meth to Account struct
    Defined AccSender interface with single meth SendToKafka.
    Changed NewAcc() return signature: return AccSender instead of Account.

--- model/account.go
+++ model/account.go
@@ -1,12 +1,23 @@
 package model
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
 	"sync"
 
+	"github.com/altnometer/account/kafka"
+	"github.com/gorilla/context"
 	"github.com/satori/uuid"
 
 	"golang.org/x/crypto/bcrypt"
 )
+
+// AccSender sends acc data to kafka.
+type AccSender interface {
+	SendToKafka(r *http.Request) error
+}
 
 // Account holds core user details.
 type Account struct {
@@ -16,7 +27,7 @@ type Account struct {
 }
 
 // NewAcc returns a Account instance
-var NewAcc = func(name, pwd string) (*Account, error) {
+var NewAcc = func(name, pwd string) (AccSender, error) {
 	acc := Account{Name: name}
 	if err := acc.initPwdHash(pwd); err != nil {
 		return nil, err
@@ -50,6 +61,23 @@ func (a *Account) initUID() error {
 func (a *Account) CheckPwdHash(pwd string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(a.PwdHash), []byte(pwd))
 	return err == nil
+}
+
+// SendToKafka sends an account data to a kafka topic.
+func (a *Account) SendToKafka(r *http.Request) error {
+	k, ok := context.GetOk(r, "kfkProdr")
+	if !ok {
+		return errors.New("NO_KAFKA_PRODUCER_IN_CONTEXT")
+	}
+	accBytes, err := json.Marshal(&a)
+	if err != nil {
+		return err
+	}
+	kp := k.(kafka.ISyncProducer)
+	if err := kp.SendAccMsg(a.ID, string(accBytes)); err != nil {
+		return fmt.Errorf("FAILED_KAFKA_MSG_SEND: %s", err.Error())
+	}
+	return nil
 }
 
 type uNameSet struct {
