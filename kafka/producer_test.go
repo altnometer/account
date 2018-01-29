0121e9a37b20fbbebb255f5d54f712a61d99f5c3
sam <sam@redmoo.info>
2018-01-29 12:56:50 +0300

    Modified behavior msgs in tests for kafka producer

--- kafka/producer_test.go
+++ kafka/producer_test.go
@@ -8,29 +8,27 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Kafka SyncProducer", func() {
-	Describe("InitMySyncProducer", func() {
-		var (
-			brokersEnvVar string
-		)
-		BeforeEach(func() {
-			brokersEnvVar = "127.0.0.1:9092,127.0.0.1:9092"
-		})
+var _ = FDescribe("Kafka SyncProducer", func() {
+	var (
+		brokersEnvVar string
+	)
+	BeforeEach(func() {
+		brokersEnvVar = "127.0.0.1:9092,127.0.0.1:9092"
+	})
+	JustBeforeEach(func() {
+		os.Setenv("KAFKA_BROKERS", brokersEnvVar)
+	})
+	Context("no KAFKA_BROKERS env var is set", func() {
 		JustBeforeEach(func() {
-			os.Setenv("KAFKA_BROKERS", brokersEnvVar)
+			os.Unsetenv("KAFKA_BROKERS")
 		})
-		Context("when no KAFKA_BROKERS env var is set", func() {
-			JustBeforeEach(func() {
-				os.Unsetenv("KAFKA_BROKERS")
-			})
-			It("panics with a correct msg", func() {
-				defer func() {
-					r := recover()
-					Expect(r).NotTo(BeNil())
-					Expect(r).Should(ContainSubstring("NO_KAFKA_BROKERS_ARG_IN_ENV"))
-				}()
-				_ = kafka.NewSyncProducer()
-			})
+		It("panics with a correct msg", func() {
+			defer func() {
+				r := recover()
+				Expect(r).NotTo(BeNil())
+				Expect(r).Should(ContainSubstring("NO_KAFKA_BROKERS_ARG_IN_ENV"))
+			}()
+			_ = kafka.NewSyncProducer()
 		})
 	})
 })
