6b473e4ff187fea91f3ee4be381562ee188d89b0
sam <sam@redmoo.info>
2018-01-31 19:51:43 +0300

    Drafted kafka consumer for account msgs

---
+++ kafka/consumer.go
@@ -0,0 +1,149 @@
+package kafka
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+
+	"github.com/Shopify/sarama"
+)
+
+// IConsumer consumes account msgs.
+type IConsumer interface {
+	ConsumeMsgs(msgHandler) error
+}
+
+type msgHandler func(key, val []byte) error
+
+// Consumer implements IConsumer interface.
+type Consumer struct {
+	brokers []string
+	Consr   sarama.Consumer
+	Done    chan bool
+}
+
+var accConsr *Consumer
+
+var onceInitCons sync.Once
+
+// NewConsumer returns a pointer to an initialized instance of Consumer.
+var NewConsumer = func() IConsumer {
+	onceInitCons.Do(func() {
+		accConsr = &Consumer{}
+		if err := accConsr.initMyConsumer(); err != nil {
+			panic(err.Error())
+		}
+	})
+	return accConsr
+}
+
+func (c *Consumer) getBrokers() error {
+	brokersStr := os.Getenv("KAFKA_BROKERS")
+	if len(brokersStr) == 0 {
+		return errors.New("NO_KAFKA_BROKERS_ARG_IN_ENV")
+	}
+	c.brokers = strings.Split(brokersStr, ",")
+	return nil
+}
+
+func (c *Consumer) initMyConsumer() error {
+	if err := c.getBrokers(); err != nil {
+		return err
+	}
+	var err error
+	if c.Consr, err = sarama.NewConsumer(c.brokers, newKafkaConf()); err != nil {
+		return err
+	}
+	partitionList, err := c.Consr.Partitions(topic)
+	if err != nil {
+		return fmt.Errorf("Kafka consumer failed to get "+
+			"partitions, topic: %s, err: %s\n", topic, err)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(partitionList))
+
+	done := make(chan bool, 1)
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	signal.Notify(ch, os.Kill)
+	for _, partition := range partitionList {
+		p := partition
+		pc, err := c.Consr.ConsumePartition(
+			topic, p, sarama.OffsetOldest)
+		if err != nil {
+			return fmt.Errorf("ConsumePartition() failed, topic %s, "+
+				"partition %v, err: %s", topic, p, err)
+		}
+		go func(pc sarama.PartitionConsumer) {
+			for err := range pc.Errors() {
+				fmt.Printf(
+					"Kafka consumer, topic: %s, partition %v, "+
+						"err: %s\n", topic, p, err)
+			}
+		}(pc)
+		go func() {
+			defer wg.Done()
+			<-ch
+			fmt.Printf("Closing partitionConsumer, topic: %s, "+
+				"partition: %v\n", topic, p)
+			pc.AsyncClose()
+			// if err := pc.Close(); err != nil {
+			// 	fmt.Printf("Failed closing topic: %s, partion %v, "+
+			// 		"err: %s", topic, p, err)
+			// }
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+	select {
+	case <-done:
+		if err := c.Consr.Close(); err != nil {
+			fmt.Printf("Failed closing kafka Consumer with err: %s\n", err)
+			panic("Failed closing kafka Consumer, err: " + err.Error())
+		}
+		fmt.Println("kafka Consumer is closed")
+		os.Exit(0)
+	}
+	return nil
+}
+
+// ConsumeMsgs consumes kafka msgs and feeds them to msgHandler.
+func (c *Consumer) ConsumeMsgs(handler msgHandler) error {
+	partitionList, err := c.Consr.Partitions(topic)
+	if err != nil {
+		return fmt.Errorf("Kafka consumer failed to get partitions "+
+			"list for topic %s\n", topic)
+	}
+
+	errChan := make(chan error, 1)
+	for _, partition := range partitionList {
+		pc, err := c.Consr.ConsumePartition(
+			topic, partition, sarama.OffsetOldest)
+		if err != nil {
+			return fmt.Errorf("ConsumePartition() failed, topic %s, "+
+				"partition %v, err: %s", topic, partition, err.Error())
+		}
+		go func(pc sarama.PartitionConsumer) {
+			for msg := range pc.Messages() {
+				if err := handler(msg.Key, msg.Value); err != nil {
+					errChan <- fmt.Errorf("handler(msg) failed for topic %s, "+
+						"partition: %v, offset: %v, err: %s\n",
+						msg.Topic, msg.Partition, msg.Offset, err)
+					//close pc.AsyncClose() // c.Consr.Close()
+				}
+			}
+		}(pc)
+	}
+	select {
+	case <-c.Done:
+		return nil
+	case err := <-errChan:
+		return err
+	}
+}
---
+++ kafka/consumer_test.go
@@ -0,0 +1,222 @@
+package kafka_test
+
+import (
+	"errors"
+	"os"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/altnometer/account/kafka"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type MockConsumer struct {
+	done chan bool
+
+	PartitionsCall struct {
+		Receive struct {
+			Topic string
+		}
+		Return struct {
+			PartitionList []int32
+			Error         error
+		}
+	}
+	ConsumePartitionCall struct {
+		Receives struct {
+			Topic     string
+			Partition int32
+			Offset    int64
+		}
+		Return struct {
+			PartitionConsumer sarama.PartitionConsumer
+			Error             error
+		}
+	}
+}
+
+func (c *MockConsumer) Topics() ([]string, error) {
+	return []string{"testTopic"}, nil
+}
+func (c *MockConsumer) Close() error {
+	return nil
+}
+func (c *MockConsumer) HighWaterMarks() map[string]map[int32]int64 {
+	m := make(map[string]map[int32]int64)
+	return m
+}
+
+func (c *MockConsumer) Partitions(topic string) ([]int32, error) {
+	c.PartitionsCall.Receive.Topic = topic
+	return c.PartitionsCall.Return.PartitionList, c.PartitionsCall.Return.Error
+
+}
+
+func (c *MockConsumer) ConsumePartition(
+	topic string, partition int32, offset int64) (sarama.PartitionConsumer, error) {
+	c.ConsumePartitionCall.Receives.Topic = topic
+	c.ConsumePartitionCall.Receives.Partition = partition
+	c.ConsumePartitionCall.Receives.Offset = offset
+
+	pc := c.ConsumePartitionCall.Return.PartitionConsumer
+	err := c.ConsumePartitionCall.Return.Error
+	return pc, err
+
+}
+
+type MockPartitionConsumer struct {
+	MessagesCall struct {
+		Return struct {
+			MsgsChan chan *sarama.ConsumerMessage
+		}
+	}
+}
+
+func (pc *MockPartitionConsumer) AsyncClose() {
+}
+func (pc *MockPartitionConsumer) Close() error {
+	return nil
+}
+func (pc *MockPartitionConsumer) HighWaterMarkOffset() int64 {
+	return 123
+}
+
+func (pc *MockPartitionConsumer) Messages() <-chan *sarama.ConsumerMessage {
+	return pc.MessagesCall.Return.MsgsChan
+}
+func (pc *MockPartitionConsumer) Errors() <-chan *sarama.ConsumerError {
+	return nil
+}
+
+var _ = Describe("Kafka Consumer", func() {
+	var (
+		brokersEnvVar string
+	)
+	BeforeEach(func() {
+		brokersEnvVar = "127.0.0.1:9092,127.0.0.1:9092"
+	})
+	JustBeforeEach(func() {
+		os.Setenv("KAFKA_BROKERS", brokersEnvVar)
+	})
+	Context("no KAFKA_BROKERS env var is set", func() {
+		JustBeforeEach(func() {
+			os.Unsetenv("KAFKA_BROKERS")
+		})
+		It("panics with a correct msg", func() {
+			defer func() {
+				r := recover()
+				Expect(r).NotTo(BeNil())
+				Expect(r).Should(ContainSubstring("NO_KAFKA_BROKERS_ARG_IN_ENV"))
+			}()
+			_ = kafka.NewConsumer()
+		})
+	})
+})
+
+var _ = Describe("ConsumeMsgs", func() {
+	var (
+		mc  MockConsumer
+		mpc MockPartitionConsumer
+
+		msgChan chan *sarama.ConsumerMessage
+
+		myConsr    kafka.Consumer
+		msgHandler func(key, val []byte) error
+	)
+	BeforeEach(func() {
+		mpc = MockPartitionConsumer{}
+		msgChan = make(chan *sarama.ConsumerMessage, 1)
+		mpc.MessagesCall.Return.MsgsChan = msgChan
+
+		mc = MockConsumer{}
+		mc.PartitionsCall.Return.Error = nil
+		mc.PartitionsCall.Return.PartitionList = []int32{1, 2}
+		mc.ConsumePartitionCall.Return.PartitionConsumer = sarama.PartitionConsumer(&mpc)
+
+		myConsr = kafka.Consumer{}
+		myConsr.Consr = &mc
+		myConsr.Done = make(chan bool, 1)
+		msgHandler = func(key, val []byte) error {
+			return nil
+		}
+
+	})
+	AfterEach(func() {
+		close(msgChan)
+		close(myConsr.Done)
+	})
+	JustBeforeEach(func() {
+	})
+	Context("fails to get partitions", func() {
+		JustBeforeEach(func() {
+			mc.PartitionsCall.Return.Error = errors.New("mock error")
+			mc.PartitionsCall.Return.PartitionList = nil
+		})
+		It("returns an error", func() {
+			err := myConsr.ConsumeMsgs(msgHandler)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("failed to get partitions"))
+		})
+	})
+	Context("fails to get PartitionConsumer", func() {
+		JustBeforeEach(func() {
+			mc.ConsumePartitionCall.Return.PartitionConsumer = nil
+			mc.ConsumePartitionCall.Return.Error = errors.New("mock error")
+		})
+		It("returns an error", func() {
+			err := myConsr.ConsumeMsgs(msgHandler)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("ConsumePartition() failed"))
+		})
+	})
+	Context("receives a msg", func() {
+		msgKeySend := "key1"
+		msgValSend := "val1"
+		var msgKeyRecieved []byte
+		var msgValRecieved []byte
+		JustBeforeEach(func() {
+			msgHandler = func(key, val []byte) error {
+				msgKeyRecieved = key
+				msgValRecieved = val
+
+				// return errors.New("mock error")
+				return nil
+			}
+		})
+		It("msgHandler() is called with correct args", func() {
+			go func(mc chan *sarama.ConsumerMessage) {
+				msg := sarama.ConsumerMessage{
+					Key: []byte(msgKeySend), Value: []byte(msgValSend)}
+				mc <- &msg
+				time.Sleep(time.Duration(10 * time.Millisecond))
+				myConsr.Done <- true
+			}(msgChan)
+
+			err := myConsr.ConsumeMsgs(msgHandler)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(msgKeyRecieved)).To(Equal(msgKeySend))
+			Expect(string(msgValRecieved)).To(Equal(msgValSend))
+
+		})
+	})
+	Context("msg handler returns an error", func() {
+		JustBeforeEach(func() {
+			msgHandler = func(key, val []byte) error {
+				return errors.New("mock error")
+			}
+		})
+		It("returns a correct error", func() {
+			go func(mc chan *sarama.ConsumerMessage) {
+				msg := sarama.ConsumerMessage{
+					Key: []byte("key1"), Value: []byte("val1")}
+				mc <- &msg
+			}(msgChan)
+			err := myConsr.ConsumeMsgs(msgHandler)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("handler(msg) failed"))
+
+		})
+	})
+})
