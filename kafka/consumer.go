0e81872b80d287702ae76f857baa8a4e5d014510
sam <sam@redmoo.info>
2018-02-06 11:04:02 +0300

    Rewrote kafka consumer code
    Replaced initMyConsumer() and ConsumeMsgs() methods of AccConsumer with
    ConsumeAccMsgs() function.

--- kafka/consumer.go
+++ kafka/consumer.go
@@ -1,53 +1,48 @@
 package kafka
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"os/signal"
-	"strings"
 	"sync"
 
 	"github.com/Shopify/sarama"
 )
 
-// IConsumer consumes account msgs.
-type IConsumer interface {
-	ConsumeMsgs(msgHandler) error
-}
-
 type msgHandler func(key, val []byte) error
 
-// Consumer implements IConsumer interface.
-type Consumer struct {
-	brokers []string
-	Consr   sarama.Consumer
-	Done    chan bool
+// AccConsumer handles received kafka account messages.
+type AccConsumer struct {
+	Consr sarama.Consumer
+
+	DoneTesting chan bool
 }
 
-var accConsr *Consumer
+var accConsr *AccConsumer
 
 var onceInitCons sync.Once
 
-// GetConsumer returns a pointer to an initialized instance of Consumer.
-var GetConsumer = func() IConsumer {
+// GetConsumer returns a pointer to an initialized instance of AccConsumer.
+var GetConsumer = func() *AccConsumer {
 	onceInitCons.Do(func() {
-		accConsr = &Consumer{}
-		if err := accConsr.initMyConsumer(); err != nil {
+		accConsr = &AccConsumer{}
+		var err error
+		var brokers []string
+		if brokers, err = getBrokers(); err != nil {
+			panic(err.Error())
+		}
+		if accConsr.Consr, err = sarama.NewConsumer(
+			brokers, newKafkaConf()); err != nil {
 			panic(err.Error())
 		}
 	})
 	return accConsr
 }
 
-func (c *Consumer) initMyConsumer() error {
-	if err := c.getBrokers(); err != nil {
-		return err
-	}
+// ConsumeAccMsgs receive and handle kafka account messages.
+func ConsumeAccMsgs(handler msgHandler) error {
+	c := GetConsumer()
 	var err error
-	if c.Consr, err = sarama.NewConsumer(c.brokers, newKafkaConf()); err != nil {
-		return err
-	}
 	partitionList, err := c.Consr.Partitions(topic)
 	if err != nil {
 		return fmt.Errorf("Kafka consumer failed to get "+
@@ -55,9 +50,11 @@ func (c *Consumer) initMyConsumer() error {
 	}
 
 	var wg sync.WaitGroup
-	wg.Add(len(partitionList))
 
-	done := make(chan bool, 1)
+	done := make(chan interface{}, 1)
+	completed := make(chan interface{}, 1)
+	withErr := make(chan error, 1)
+	errChan := make(chan error, 1)
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
 	signal.Notify(ch, os.Kill)
@@ -66,75 +63,66 @@ func (c *Consumer) initMyConsumer() error {
 		pc, err := c.Consr.ConsumePartition(
 			topic, p, sarama.OffsetOldest)
 		if err != nil {
-			return fmt.Errorf("ConsumePartition() failed, topic %s, "+
-				"partition %v, err: %s", topic, p, err)
+			go func() {
+				errChan <- fmt.Errorf("ConsumePartition() failed, topic %s, "+
+					"partition %v, err: %s", topic, p, err)
+			}()
 		}
-		go func(pc sarama.PartitionConsumer) {
-			for err := range pc.Errors() {
-				fmt.Printf(
-					"Kafka consumer, topic: %s, partition %v, "+
-						"err: %s\n", topic, p, err)
-			}
-		}(pc)
-		go func() {
-			defer wg.Done()
-			<-ch
-			fmt.Printf("Closing partitionConsumer, topic: %s, "+
-				"partition: %v\n", topic, p)
-			pc.AsyncClose()
-			// if err := pc.Close(); err != nil {
-			// 	fmt.Printf("Failed closing topic: %s, partion %v, "+
-			// 		"err: %s", topic, p, err)
-			// }
-		}()
+		if pc != nil {
+			wg.Add(1)
+			go func(pc sarama.PartitionConsumer) {
+				for err := range pc.Errors() {
+					fmt.Printf(
+						"Kafka consumer, topic: %s, partition %v, "+
+							"err: %s\n", topic, p, err)
+				}
+			}(pc)
+			go func(pc sarama.PartitionConsumer) {
+				for msg := range pc.Messages() {
+					if err := handler(msg.Key, msg.Value); err != nil {
+						errChan <- fmt.Errorf("msg handler failed for topic %s, "+
+							"partition: %v, offset: %v, err: %s\n",
+							msg.Topic, msg.Partition, msg.Offset, err)
+					}
+				}
+			}(pc)
+			go func(pc sarama.PartitionConsumer) {
+				defer wg.Done()
+				<-done
+				fmt.Printf("Closing partitionConsumer, topic: %s, "+
+					"partition: %v\n", topic, p)
+				pc.AsyncClose()
+				// if err := pc.Close(); err != nil {
+				// 	fmt.Printf("Failed closing topic: %s, partion %v, "+
+				// 		"err: %s", topic, p, err)
+				// }
+			}(pc)
+		}
 	}
 	go func() {
-		wg.Wait()
-		close(done)
+		select {
+		case <-ch:
+			close(done)
+			wg.Wait()
+			close(completed)
+		case err := <-errChan:
+			close(done)
+			wg.Wait()
+			withErr <- err
+		}
 	}()
 	select {
-	case <-done:
+	case <-completed:
 		if err := c.Consr.Close(); err != nil {
 			fmt.Printf("Failed closing kafka Consumer with err: %s\n", err)
 			panic("Failed closing kafka Consumer, err: " + err.Error())
 		}
-		fmt.Println("kafka Consumer is closed")
+		fmt.Println("kafka Consumer is closed after os.signal")
 		os.Exit(0)
+	case err := <-withErr:
+		return err
+	case <-c.DoneTesting: // no need to close mock consumers.
+		return nil
 	}
 	return nil
-}
-
-// ConsumeMsgs consumes kafka msgs and feeds them to msgHandler.
-func (c *Consumer) ConsumeMsgs(handler msgHandler) error {
-	partitionList, err := c.Consr.Partitions(topic)
-	if err != nil {
-		return fmt.Errorf("Kafka consumer failed to get partitions "+
-			"list for topic %s\n", topic)
-	}
-
-	errChan := make(chan error, 1)
-	for _, partition := range partitionList {
-		pc, err := c.Consr.ConsumePartition(
-			topic, partition, sarama.OffsetOldest)
-		if err != nil {
-			return fmt.Errorf("ConsumePartition() failed, topic %s, "+
-				"partition %v, err: %s", topic, partition, err.Error())
-		}
-		go func(pc sarama.PartitionConsumer) {
-			for msg := range pc.Messages() {
-				if err := handler(msg.Key, msg.Value); err != nil {
-					errChan <- fmt.Errorf("handler(msg) failed for topic %s, "+
-						"partition: %v, offset: %v, err: %s\n",
-						msg.Topic, msg.Partition, msg.Offset, err)
-					//close pc.AsyncClose() // c.Consr.Close()
-				}
-			}
-		}(pc)
-	}
-	select {
-	case <-c.Done:
-		return nil
-	case err := <-errChan:
-		return err
-	}
 }
