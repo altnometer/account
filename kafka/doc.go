ba06b33e713f0483a8e06117102c32a713728fc9
sam <sam@redmoo.info>
2018-02-08 12:43:18 +0300

    Updated kafka/doc.go

--- kafka/doc.go
+++ kafka/doc.go
@@ -5,10 +5,6 @@
 // A kafka consumer usage.
 //
 // Call
-// err := ConsumeAccMsgs(handler msgHandler)
-// where msgHandler type is func(key, val []byte) error.
-// ConsumeAccMsgs() blocks execution. Execute from a goroutine if required.
-// An error returned by msgHadler would shutdown the consumer that can be
-// initialize only once. You need to restart the process after the error.
+// TODO: fill in the instructions
 //
 package kafka
